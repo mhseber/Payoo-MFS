aa0e743d2da5c3f169c0c6f37f540506209b90fa
mahamudul hasan seber <sebermh@gmail.com>
2024-09-19 02:04:12 +0600

    Cash Out add

---
+++ js/cashout.js
@@ -0,0 +1,25 @@
+// console.log('go to go')
+
+document.getElementById('btn-cash-out').addEventListener('click', function (event) {
+    event.preventDefault();
+    console.log('cash out button clicked')
+
+    const cashOut = document.getElementById('input-cash-out').value;
+    const cashOutNumber = parseFloat(cashOut);
+    const pinNumber = document.getElementById('input-cash-out-pin').value;
+    console.log(cashOut, pinNumber);
+
+    if (pinNumber === '1234') {
+        const balance = document.getElementById('account-balance').innerText;
+        const balanceNumber = parseFloat(balance);
+
+        // reduce the balance 
+        const newBalance = balanceNumber - cashOutNumber;
+
+        // update the ui 
+        document.getElementById('account-balance').innerText = newBalance;
+    }
+    else {
+        alert('Please Try Agin')
+    }
+})
