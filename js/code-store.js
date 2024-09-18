80f5a58b2aa016cee958737129784fc4eadd25f8
mahamudul hasan seber <sebermh@gmail.com>
2024-09-18 21:50:33 +0600

    Payoo Home Page

---
+++ js/code-store.js
@@ -0,0 +1,25 @@
+// console.log('Everything is ok let go')
+
+// document.getElementById('id').addEventListener('click', function)
+//step - 1 set event handler
+document.getElementById('button-login').addEventListener('click', function (event) {
+    // step - 2 prevent default behavior (prevent reloading browser)
+    event.preventDefault();
+    // console.log('login button clicked');
+
+
+    // step - 3 get the phone number and the pin number
+    const phoneNumber = document.getElementById('phone-number').value;
+    const pinNumber = document.getElementById('pin-number').value;
+
+    console.log(phoneNumber, pinNumber)
+    //step - 4 validate phone and pin 
+    // this is temporary. You should do like this
+    if (phoneNumber === '5' && pinNumber === '1234') {
+        console.log('you are logged in')
+        //step - 5 allow user to user the website
+    }
+    else {
+        alert('wrong phone number or pin')
+    }
+})
--- js/login.js
+++ js/login.js
@@ -1,16 +1,43 @@
-// console.log('Everything is ok let go')
+// // console.log('Everything is ok lets go')
 
-// document.getElementById('id').addEventListener('click', function)
-//step - 1 set event handler
+// // document.getElementById('id').addEventListener('click', function)
+// //step - 1 set event handler
+// document.getElementById('button-login').addEventListener('click', function (event) {
+//     // step - 2 prevent default behavior (prevent reloading browser)
+//     event.preventDefault();
+//     // console.log('login button clicked');
+
+
+//     // step - 3 get the phone number and the pin number
+//     const phoneNumber = document.getElementById('phone-number').value;
+//     const pinNumber = document.getElementById('pin-number').value;
+
+//     console.log(phoneNumber, pinNumber)
+//     //step - 4 validate phone and pin
+//     // this is temporary. You should do like this
+//     if (phoneNumber === '5' && pinNumber === '1234') {
+//         console.log('you are logged in')
+//         //step - 5 allow user to user the website
+//     }
+//     else {
+//         alert('wrong phone number or pin')
+//     }
+// })
+
 document.getElementById('button-login').addEventListener('click', function (event) {
-    // step - 2 prevent default behavior (prevent reloading browser)
     event.preventDefault();
-    console.log('login button clicked');
 
-
-    // step - 3 get the phone number
+    // get phone number and pin
     const phoneNumber = document.getElementById('phone-number').value;
-    console.log(phoneNumber)
+    const pinNumber = document.getElementById('pin-number').value;
+    // console.log(phoneNumber, pinNumber);
 
-
-})
+    //bad way to validate
+    if (phoneNumber === '5' && pinNumber === '1234') {
+        console.log('you are logged in')
+        window.location.href = '/home.html';
+    }
+    else {
+        alert('wrong phone number or pin')
+    }
+});
