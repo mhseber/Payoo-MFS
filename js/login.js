bde7c4ba8ed822ec7a12990ead50fce3ba5b2260
mahamudul hasan seber <sebermh@gmail.com>
2024-09-18 19:46:57 +0600

    prevent default behavior

---
+++ js/login.js
@@ -0,0 +1,16 @@
+// console.log('Everything is ok let go')
+
+// document.getElementById('id').addEventListener('click', function)
+//step - 1 set event handler
+document.getElementById('button-login').addEventListener('click', function (event) {
+    // step - 2 prevent default behavior (prevent reloading browser)
+    event.preventDefault();
+    console.log('login button clicked');
+
+
+    // step - 3 get the phone number
+    const phoneNumber = document.getElementById('phone-number').value;
+    console.log(phoneNumber)
+
+
+})
